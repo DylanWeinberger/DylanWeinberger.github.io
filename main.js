348b2010892e6305df28a24304f04165ac9ffb09
Dylan Weinberger <djweinbe@gmail.com>
2016-03-07 21:11:53 -0500

    Adding Existing Portfolio site

---
+++ main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+     $(".scroll").click(function(event){
+         event.preventDefault();
+         
+         var dest=0;
+         if($(this.hash).offset().top > $(document).height()-$(window).height()){
+              dest=$(document).height()-$(window).height();
+         }else{
+              dest=$(this.hash).offset().top;
+         }
+         //go to destination
+         $('html,body').animate({scrollTop:dest}, 1000,'swing');
+     });
+})
